dfc2fa5a4ce9de605c69a4646b46bf5623a4006d
cormie45 <steve.cormack45@gmail.com>
2021-06-09 12:40:49 +0100

    heroku setup

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
-
+const path = require("path");
 const cors = require("cors");
-app.use(cors());
 
+app.use(cors());
+app.use(express.static(path.join(__dirname, "client", "build")));
 app.use(express.json());
 
 const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./helpers/create_router.js');
+const port = process.env.PORT || 5000;
 
-MongoClient.connect('mongodb://localhost:27017')
+MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .then((client) => {
     const db = client.db('snakes_and_ladders');
     const taskCollection = db.collection('tasks');
@@ -20,6 +22,10 @@ MongoClient.connect('mongodb://localhost:27017')
     app.use(('/api/actions'), taskRouter2);
 });
 
-app.listen(5000, function() {
-    console.log(`Server is now running`)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(port, function() {
+    console.log(`Server is now running`);
 });
